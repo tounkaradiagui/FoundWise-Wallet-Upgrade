54b7787405eeebec6817a595d2c38964661ed607
Diagui Tounkara <tounkaradiagui@gmail.com>
2025-07-11 10:38:14 +0300

    API check on production

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,12 +4,18 @@ import dotenv from 'dotenv';
 import ratelimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
 import { initDB } from './controllers/transactionsController.js';
+import job from './config/cron.js';
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Create an Express application
 const app = express();
+
+// Start the cron job
+if(process.env.NODE_ENV === "production") {
+  job.start(); 
+};
 
 // Middleware
 // app.use(ratelimiter); 
@@ -19,6 +25,11 @@ app.use(express.json());
 // Set the port from environment variable or default to 3000
 const PORT = process.env.PORT || 3000;
 
+// This endpoint is used to check if the backend is healthy
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'Server is healthy' });
+}); 
+ 
 // API endpoint to check if the backend is working
 app.get('/', (req, res) => {
   res.send('Backend is working!');
