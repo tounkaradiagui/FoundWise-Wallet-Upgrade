08d476b4d4b134b1a85dc0e7d5f1431245989fba
Diagui Tounkara <tounkaradiagui@gmail.com>
2025-07-15 18:55:56 +0300

    Latest update 15/07/2025

--- Backend/src/controllers/transactionsController.js
+++ Backend/src/controllers/transactionsController.js
@@ -7,7 +7,7 @@ export async function initDB() {
       id SERIAL PRIMARY KEY,
       user_id VARCHAR(255) NOT NULL,
       title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10,2) NOT NULL,
+      amount DECIMAL(10) NOT NULL,
       category VARCHAR(255) NOT NULL,
       created_at DATE NOT NULL DEFAULT CURRENT_DATE
     )`;
